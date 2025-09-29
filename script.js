d34370a388ed8f776a8995d0629e4aef72f05cec
ollewarne <209520777+ollewarne@users.noreply.github.com>
2025-09-29 17:06:01 +0200

    add button to clear everything from the display

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const calculatorDisplay = document.getElementById("calculator-display");
 const numberButtons = document.getElementsByClassName("calculator-button");
 
+//TODO: add a way to track the operator last used
+//TODO: add variable to track the last number that was on the display
+
 for (let i = 0; i < numberButtons.length; i++) {
     numberButtons[i].addEventListener('click', () => {
         calculatorDisplay.textContent += numberButtons[i].textContent;
@@ -45,4 +48,9 @@ function calculate(operator, firstNumber, secondNumber) {
 const eraseButton = document.getElementById("erase-calc-button");
 eraseButton.addEventListener('click', () => {
     calculatorDisplay.textContent = calculatorDisplay.textContent.slice(0, -1);
+});
+
+const clearButton = document.getElementById("clear-display-button");
+clearButton.addEventListener('click', () => {
+    calculatorDisplay.textContent = "";
 });
